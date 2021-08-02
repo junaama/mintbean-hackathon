6a01f8b9af91956ea63ffd35397775d76c5354d0
junaama <0naama0@gmail.com>
2021-08-01 19:23:04 -0700

    setup api with auth0

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mintbean-hackathon
+- Egyptian Ratscrew (ERS) or Slap.
+
+## technologies
+- auth0
+- javascript - node.js
---
+++ server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require('express')
+const app = express();
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: 'http://localhost:3000',
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: 'https://junaama.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
+
+app.listen(3000, () => {
+
+    console.log("ERS listening on port: 3000");
+})
+
